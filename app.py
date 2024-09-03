8ee07b553763d6b87200be6cf73d9f652e416c82
yogeshwarans-abd <yogeshwarangsm1@gmail.com>
2024-09-03 07:33:53 +0530

    03.09.2024
    Added the logic to register a user and sign-in as a user

--- app.py
+++ app.py
@@ -14,13 +14,39 @@ def signin():
         return render_template('signin.html')
     if request.method=='POST':
         data = request.form
-        return render_template('index.html')
+        name = data['username']
+        passwd = data['password']
+        flag=user_check(name, passwd)
+        if flag[0]==1:
+            return redirect(url_for('dashboard',user=flag[1]))
+        return render_template('failed.html', content=flag)
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     if request.method=='GET':
         return render_template('signup.html')
+    if request.method=='POST':
+        data = request.form
+        if is_unique_user(data['username'],data['email']):
+            if data['password']==data['rpassword']:
+                update_db([ data['fullname'], data['username'], data['email'], encrypt(data['password'])])
+                return render_template('failed.html', content='Registerd Successfully. Please Login again')
+            else:
+                return render_template('failed.html', content='Password and Re-Type password didn\'t match. Please try again')
+        else:
+            return render_template('failed.html', content='The User is already registerd. Contact Admin to channge or get a new password!')
 
+@app.route('/change-password', methods=['GET','POST'])
+def change_password():
+    if request.method=='GET':
+        return render_template('passwd.html')
+    if request.method=='POST':
+        data  = request.form
+        return f"the data given is {data}"
+
+@app.route('/<user>/dashboard')
+def dashboard(user):
+    return f'<h1> Welcome {user} </h1>'
 
 if __name__=='__main__':
     app.run(debug=True)
--- static/header.js
+++ static/header.js
@@ -2,11 +2,13 @@ content = `
         <header>
     		<nav>
                 <figure>
-                    <img src="./static/logo.gif" alt="Logo gif Goes here!">
-                    <div>
-                        <h1>Flexi Cloud</h1>
-                        <em>Empower Your Data with Flexi Cloud</em>
-                    </div>
+                    <a href="/" id="header">
+                        <img src="./static/logo.gif" alt="Logo gif Goes here!">
+                        <div>
+                            <h1>Flexi Cloud</h1>
+                            <em>Empower Your Data with Flexi Cloud</em>
+                        </div>
+                    </a>
                 </figure>
                 <div class="nav-links">
                     <a href="/#about" class="btns">About Us</a>
